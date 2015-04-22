fa1d5ac3394488055a2470c121ae554c0c5fe2cd
Caleb Callaway <cqcallaw@gmail.com>
2015-04-21 21:34:55 -0700

    style normalizations

--- js/local.js
+++ js/local.js
@@ -1,1 +1,12 @@
 var availableTags = [ "Sarah Jensen", "12345", "32452", "Billy Johnson" ];
+
+document.addEventListener("keydown", keyDownTextField, false);
+
+function keyDownTextField(e) {
+	var keyCode = e.keyCode;
+	if (keyCode == 113) {
+		alert('fuk yea');
+		// ref: http://stackoverflow.com/a/480754/577298
+		$("#search").focus();
+	}
+}
