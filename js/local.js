a900f88266a75b7e8306860e93aa73e65a4ffe4d
Caleb Callaway <cqcallaw@gmail.com>
2015-04-22 20:43:39 -0700

    search

--- js/local.js
+++ js/local.js
@@ -1,36 +1,21 @@
 var availableTags = [ "Sarah Jensen", "12345", "32452", "Billy Johnson" ];
 
-//------------------- User order Info -------------------------------
-var peoplesNames = [
-	"Billy Johnson",
-	"Scarlet Johanson",
-	"Sarah Jensen",
-    ];
+// ------------------- User order Info -------------------------------
+var peoplesNames = [ "Billy Johnson", "Scarlet Johanson", "Sarah Jensen", ];
 
-var billingAddress = [
-	["123 Main st", "Chico", "95926", "CA"],
-	["123 Nord", "Chico", "54321", "NY"],
-	["626 B st", "Los Angles", "95933", "CA"]
-	];
+var billingAddress = [ [ "123 Main st", "Chico", "95926", "CA" ],
+		[ "123 Nord", "Chico", "54321", "NY" ],
+		[ "626 B st", "Los Angles", "95933", "CA" ] ];
 
-var shippingAddress = [
-	["123 Main st", "Chico", "95926"],
-	["123 Nord", "Chico", "54321"],
-	["626 B st", "Los Angles", "95933"]
-	];
+var shippingAddress = [ [ "123 Main st", "Chico", "95926" ],
+		[ "123 Nord", "Chico", "54321" ], [ "626 B st", "Los Angles", "95933" ] ];
 
-var paymentInfo = [
-	["Visa", "****6453", "09/12"],
-	["Amex", "****7763", "02/18"],
-	["Master Card", "****9944", "11/20"]
-	];
+var paymentInfo = [ [ "Visa", "****6453", "09/12" ],
+		[ "Amex", "****7763", "02/18" ], [ "Master Card", "****9944", "11/20" ] ];
 
-var userPrefrences = [
-	["No", "Yes", "Yes", "No"],
-	["No", "Yes", "Yes", "No"],
-	["Yes", "Yes", "Yes", "No"]
- 	];
-//------------------------------------------------------------------------
+var userPrefrences = [ [ "No", "Yes", "Yes", "No" ],
+		[ "No", "Yes", "Yes", "No" ], [ "Yes", "Yes", "Yes", "No" ] ];
+// ------------------------------------------------------------------------
 
 document.addEventListener("keyup", documentKeyUp, false);
 
@@ -51,6 +36,14 @@ function searchKeyup(event) {
 	} else {
 		var search = $("#search").val();
 		console.log("search term: " + search);
-		$("#content").text(search);
+		if (search == "Sarah Jensen") {
+			$("#content").load("sarah_jensen.html");
+		} else if (search == "Billy Johnson") {
+			$("#content").load("billy_johnson.html");
+		} else if (search == "12345") {
+			$("#content").load("12345.html");
+		} else if (search == "32452") {
+			$("#content").load("32452.html");
+		}
 	}
 }
